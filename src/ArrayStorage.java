3bcf97ebb6291e15aee155f09889e9763134ee2e
MaksPozdeev <forman1427@gmail.com>
2019-07-17 22:33:29 +0400

    The first decision of the introductory task

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,67 @@
 /**
  * Array based storage for Resumes
+ *  @autor Maksim P.
+ *  @version 0.1
+ *  @lastChanges (2019.07.17) Maksim P.
  */
 public class ArrayStorage {
+
+    /**Numbers of entries in the array*/
+    private int countResume = 0;
+
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < countResume; i++) {
+            storage[i] = null;
+        }
+        countResume = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[countResume] = resume;
+        countResume++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)){
+                resume = storage[i];
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        int position = -1;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)){
+                position = i;
+                break;
+            }
+        }
+        if (position != -1) {
+            for (int i = position; i < size(); i++) {
+                storage[i] = storage[i + 1];
+            }
+            countResume--;
+        }else {
+            System.out.println("uuid not found");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+//        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        System.arraycopy (storage, 0, resumes, 0, size());
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return countResume;
     }
 }
